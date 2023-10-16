6efda3548d900ccedff97d357aef7599092478fb
revonpro <115627602+revonpro@users.noreply.github.com>
2023-10-16 10:36:40 +0300

    Update idex.php

--- idex.php
+++ idex.php
@@ -1,3 +1,26 @@
+<!DOCTYPE html>
 <html>
-  <H1>see leht on testimiseks tehtud</H1>
+<head>
+    <meta charset="UTF-8">
+    <title>My Simple Webpage</title>
+</head>
+<body>
+    <header>
+        <h1>Welcome to My Simple Webpage</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    <main>
+        <h2>About Us</h2>
+        <p>This is a simple webpage created with HTML.</p>
+    </main>
+    <footer>
+        <p>&copy; 2023 My Simple Webpage</p>
+    </footer>
+</body>
 </html>
